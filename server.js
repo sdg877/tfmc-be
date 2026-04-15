6d6e7fa84093ad08f3f858be39e591e4c19cba45
Sdg877 <sdrakegill@gmail.com>
2026-04-15 07:24:50 +0100

    Update to cors policy to allow access from frontend.

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use("/users", userRoutes);
-app.use('/tasks', require('./routes/taskRoutes'));
+app.use("/tasks", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
