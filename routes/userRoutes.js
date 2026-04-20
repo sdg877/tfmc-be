9eb45f518a3b3c2d1f328b098f30713b890e5e3f
Sdg877 <sdrakegill@gmail.com>
2026-04-20 13:34:43 +0100

    Update to routes to include profile to allow for users energy to be updated.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
+const {
+  registerUser,
+  loginUser,
+  updateUserEnergy,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.post('/register', registerUser); 
-router.post('/login', loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+router.put("/profile", protect, updateUserEnergy);
 
 module.exports = router;
