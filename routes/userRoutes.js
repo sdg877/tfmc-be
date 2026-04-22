0fef6ab63cb5b1ede08932c7773938c452fe4c5d
Sdg877 <sdrakegill@gmail.com>
2026-04-22 07:19:10 +0100

    Route added for profile to ensure energy limit is correctly updated.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,14 @@ const {
   registerUser,
   loginUser,
   updateUserEnergy,
+  getProfile,
 } = require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
+router.get("/profile", protect, getProfile);
 router.put("/profile", protect, updateUserEnergy);
 
 module.exports = router;
