3fd9d5b27c06d731b48dd769eda2f0c62f2a31b1
Sdg877 <sdrakegill@gmail.com>
2026-04-22 07:18:27 +0100

    Model updated to allow for energy capacity to be updated and to reset energy at midnight.

--- models/userModel.js
+++ models/userModel.js
@@ -2,28 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, "Please add a name"],
-    },
-    email: {
-      type: String,
-      required: [true, "Please add an email"],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Please add a password"],
-    },
-    dailyEnergyLimit: {
-      type: Number,
-      default: 100,
-      min: 0,
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    dailyEnergyLimit: { type: Number, default: 100 },
+    lastResetDate: { type: String, default: "" },
   },
-  {
-    timestamps: true,
-  },
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("User", userSchema);
