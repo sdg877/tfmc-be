a3c2c08341333286e280513c58053dea28ea56ac
Sdg877 <sdrakegill@gmail.com>
2026-07-29 07:00:55 +0100

    Formatting test files for readability.

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,5 @@
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongoose = require('mongoose');
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require("mongoose");
 
 let mongoServer;
 
