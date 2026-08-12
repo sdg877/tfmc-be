f8cb1adb05c10b859af1669ad2d3816f26520643
Sdg877 <sdrakegill@gmail.com>
2026-08-12 05:43:21 +0100

    Update to mongoDB import statement.

--- server.js
+++ server.js
@@ -1,37 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const connectDB = require("./config/db");
-
-// dotenv.config();
-
-// connectDB();
-
-// const app = express();
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   }),
-// );
-
-// app.use(express.json());
-
-// app.use("/users", require("./routes/userRoutes"));
-// app.use("/tasks", require("./routes/taskRoutes"));
-
-// app.get("/", (req, res) => {
-//   res.send("Fast Minds Club API is running...");
-// });
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-//   console.log(`Google Redirect URI: ${process.env.GOOGLE_REDIRECT_URI}`);
-// });
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -39,8 +5,7 @@ const connectDB = require("./config/db");
 
 dotenv.config();
 
-// Fail fast if critical env vars are missing
-const requiredEnvVars = ["JWT_SECRET", "MONGO_URI"];
+const requiredEnvVars = ["JWT_SECRET", "DATABASE_URL"];
 requiredEnvVars.forEach((key) => {
   if (!process.env[key]) {
     console.error(`Missing required env var: ${key}`);
